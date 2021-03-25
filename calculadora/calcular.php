0e9cff89d52fe32901aedc3b5078a0e179a5cb9b
Vitor Hugo <62478033+Mrtorugo@users.noreply.github.com>
2021-03-25 19:32:08 -0300

    Update calcular.php

--- calculadora/calcular.php
+++ calculadora/calcular.php
@@ -1,20 +1,17 @@
 <?php
-    //Declara as vari�veis e captura os dados do formul�rio
+   
 $valor1 = $_POST['valor1'];
 $valor2 =  $_POST['valor2'];
 $tipo =  $_POST['tipo'];
  
-/* Cria uma fun��o com o nome "calcular" e define como
-seus par�metros as vari�veis $a e $b */
+
 function calcular($a,$b)
 {
-    // Declara as vari�veis como globais
+    
     global $valor1;
     global $valor2;
     global $tipo;
-    /*  Cria uma estrutura condicional com o switch e testa qual
-        o valor da vari�vel $tipo corresponde aos casos para 
-        poderem ser executados corretamente */
+  
         switch($tipo)
         {
             case 'Somar': $resultado = $a + $b; break;
@@ -22,9 +19,9 @@ function calcular($a,$b)
             case 'Multiplicar': $resultado = $a * $b; break;
             case 'Dividir': $resultado = $a / $b; break;
          }
-// Retorna o resultado do c�lculo
+
 return($resultado);
 }
-//Mostra na p�gina o resultado do c�lculo
+
 echo calcular($valor1,$valor2);
 ?>
